029864983f0753a95ad37b57c4832e842ecff4c4
Sunnat05 <abdurashidovsunnat71@gmail.com>
2023-07-05 23:50:52 +0500

    nth

--- basic_func14.py
+++ basic_func14.py
@@ -1,7 +1,11 @@
-def main(a,b):
+def main(a):
     from math import floor
-    return floor(a,b)
-print(main(3.14,3.58))   
+    return floor(a)
+print(main(3.14))  
+def maint(b):
+    from math import floor
+    return floor(b)
+print(main(5.09))
 # Create a function called main.
 # Create function arguments a and b.
 # Returns the floor division of a and b.
