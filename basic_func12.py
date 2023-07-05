06d1d948748ef3b678ea9652aa8d4de44d0649bc
Sunnat05 <abdurashidovsunnat71@gmail.com>
2023-07-05 23:12:46 +0500

    nth

--- basic_func12.py
+++ basic_func12.py
@@ -1,4 +1,7 @@
-# Create a function called main.
-# Create function argument a.
-# Return the value of a.
-# Round the result to 2 decimal places.
+def main(a):
+    return round(a,2)
+print(main(1.334))
+    # Create a function called main.
+    # Create function argument a.
+    # Return the value of a.
+    # Round the result to 2 decimal places.
