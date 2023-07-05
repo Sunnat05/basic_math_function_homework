3c16a75380305c699afad8ed5ec95e84839b0706
Sunnat05 <abdurashidovsunnat71@gmail.com>
2023-07-05 23:20:32 +0500

    nth

--- basic_func13.py
+++ basic_func13.py
@@ -1,3 +1,7 @@
-# Create a function called main.
-# Create function argument a.
-# Returns the square root of a number.
+def main(a):
+    from math import sqrt
+    return sqrt(a)
+print(main(25))
+    # Create a function called main.
+    # Create function argument a.
+    # Returns the square root of a number.
