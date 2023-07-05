2b8694e78165df321e19260fa4f5d3e2c4229ce8
Sunnat05 <abdurashidovsunnat71@gmail.com>
2023-07-06 00:13:30 +0500

    nth

--- basic_func17.py
+++ basic_func17.py
@@ -1,3 +1,7 @@
+def main(a):
+    from math import pi
+    return pi(a)
+print(main(4))
 # Create a function called main.
 # Create function arguments a.
 # Assign the value pi to the variable "a" and return.
