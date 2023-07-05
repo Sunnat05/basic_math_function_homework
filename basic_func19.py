4b597009a92672bbf00676c1a25bf50b3e5bbd3d
Sunnat05 <abdurashidovsunnat71@gmail.com>
2023-07-06 00:19:35 +0500

    nth

--- basic_func19.py
+++ basic_func19.py
@@ -1,3 +1,6 @@
+def main(a,b):
+    return abs(a-b)
+print(44,5)
 # Create a function called main.
 # Create function arguments a and b.
 # Return the absolute value of the difference between a and b.
