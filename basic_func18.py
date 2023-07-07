ca0509d5f97228af50b0f0c07e569686b2da78b4
Sunnat05 <abdurashidovsunnat71@gmail.com>
2023-07-07 09:02:34 +0500

    nth

--- basic_func18.py
+++ basic_func18.py
@@ -1,3 +1,7 @@
+def main(a):
+    from math import pi
+    a=pi
+return round(a,2)
 # Create a function called main.
 # Create function arguments a.
 # Assign the value pi to the variable "a" and return.
